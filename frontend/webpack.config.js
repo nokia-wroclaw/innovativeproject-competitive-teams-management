3b2c94fb592ac5f619dcf2e6630c8874be3c1bf6
MKaluz <michal.kaluza9@gmail.com>
2019-03-31 21:41:25 +0200

    Working frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const config = {
   entry: [
-    './index.js',
+    './src/index.js',
   ],
   module: {
     rules: [
@@ -13,8 +13,22 @@ const config = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
     ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      filename: './index.html'
+    })
+  ]
 };
-
 module.exports = config;
