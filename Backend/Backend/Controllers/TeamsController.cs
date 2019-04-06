8d030165ff43d804b6b40d70df3d487dcf38ff47
Marek <forgame2312@interia.pl>
2019-04-06 14:58:00 +0200

    Added route for displaying teams
    Just route for displaying teams without connection to database

---
+++ Backend/Backend/Controllers/TeamsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+
+        [HttpGet]
+        [HttpGet("all")]
+        public string[] GetTeams()
+        {
+            return new[]
+            {
+            "1 - Destroyers",
+            "2 - Cowards",
+            "3 - Winners"
+            };
+        }
+
+        [HttpGet("search/{team_name}")]
+        public string[] SearchTeam(string team_name)
+        {
+
+            return new[] { "There is no team " + team_name};
+
+        }
+
+    }
+}
