89eddb643a4253cad57daf714da8570818daef41
KristofWu <48284805+KristofWu@users.noreply.github.com>
2019-04-23 16:09:13 +0200

    Add files via upload

--- Frontend/src/components/Menu/Menu.js
+++ Frontend/src/components/Menu/Menu.js
@@ -1,1 +1,29 @@
+import React, { Component } from 'react';
+import './Menu.css';
 
+
+class Menu extends Component {
+    render() {
+        return (
+            <nav className="menu">
+            <h1 className="menu__logo">Nokia</h1>
+
+            <div className="menu__right">
+                <ul className="menu__list">
+                    <li className="menu__list-item"><a className="menu__link menu__link--active" href="#">Home</a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#">About</a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#">Portfolio</a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#">Contact</a></li>
+                </ul>
+
+                <button className="menu__search-button"></button>
+
+                <form className="menu__search-form hide" method="POST">
+                    <input className="menu__search-input" placeholder="Type and hit enter"/>
+                </form>
+            </div>
+        </nav>
+        )
+    }
+}
+export default Menu;
